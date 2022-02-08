b2acfc6cfcd8bf01de485378b5d0cbb23fd82829
LukaszWalencik <95505066+LukaszWalencik@users.noreply.github.com>
2022-02-08 16:29:51 +0100

    Create Cubit fot account page with signout function

--- lib/app/features/bottomnavigationbar/account/accountpage_content.dart
+++ lib/app/features/bottomnavigationbar/account/accountpage_content.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_books_to_read/app/features/bottomnavigationbar/account/cubit/account_cubit.dart';
 import 'package:my_books_to_read/app/features/login_page/login_page.dart';
+import 'package:provider/src/provider.dart';
 
 class AccountPage extends StatelessWidget {
   const AccountPage({
@@ -12,33 +15,38 @@ class AccountPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('You are login as: $email '),
-            SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-                Navigator.of(context).pop();
-
-                // context.read<AccountCubit>().logOut();
-              },
-              child: Text(
-                'Logout',
-                style: TextStyle(color: Colors.white),
+    return BlocProvider(
+      create: (context) => AccountCubit(),
+      child: BlocBuilder<AccountCubit, AccountState>(
+        builder: (context, state) {
+          return Scaffold(
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('You are login as: $email '),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      context.read<AccountCubit>().signOut();
+                      Navigator.of(context).pop();
+                    },
+                    child: Text(
+                      'Logout',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    style: ElevatedButton.styleFrom(primary: Colors.purple),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  )
+                ],
               ),
-              style: ElevatedButton.styleFrom(primary: Colors.purple),
             ),
-            SizedBox(
-              height: 20,
-            )
-          ],
-        ),
+          );
+        },
       ),
     );
   }
---
+++ lib/app/features/bottomnavigationbar/account/cubit/account_state.dart
@@ -0,0 +1,4 @@
+part of 'account_cubit.dart';
+
+@immutable
+class AccountState {}
