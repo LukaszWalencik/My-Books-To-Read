c8c1826c1a0110b17f4cc4e15c3da11d5aa2f424
LukaszWalencik <95505066+LukaszWalencik@users.noreply.github.com>
2022-02-11 14:29:33 +0100

    1

--- lib/app/data/remote_data_sources/books_remote_data_source.dart
+++ lib/app/data/remote_data_sources/books_remote_data_source.dart
@@ -1,30 +1,33 @@
 import 'package:dio/dio.dart';
 
-class BooksRemoteDataSource {
-  Future<List<Map<String, dynamic>>?> getBooksData({
-    required String bookName,
-  }) async {
-    try {
-      final response = await Dio().get<List<dynamic>>(
-          'https://openlibrary.org/search/authors.json?q=$bookName');
-      final listDynamic = response.data;
-
-      print(response.data);
-      if (listDynamic == null) {
-        return null;
-      }
-      return listDynamic.map((e) => e as Map<String, dynamic>).toList();
-    } on DioError catch (error) {
-      throw Exception(
-          error.response?.data['error']['message'] ?? 'Unknown error');
-    }
-  }
-}
-
 // class BooksRemoteDataSource {
-//   Future<Map<String, dynamic>> getBooksData({
+//   Future<List<Map<String, dynamic>>?> getBooksData({
 //     required String bookName,
 //   }) async {
-//     final response = await Dio().get<Map<String, dynamic>>(
-//         'http://openlibrary.org/search.json?q=$bookName');
-//     return response.data!;
+//     try {
+//       final response = await Dio().get<List<dynamic>>(
+//           'https://openlibrary.org/search/authors.json?q=lord');
+//       final listDynamic = response.data;
+
+//       print(response.data);
+//       if (listDynamic == null) {
+//         return null;
+//       }
+//       return listDynamic.map((e) => e as Map<String, dynamic>).toList();
+//     } on DioError catch (error) {
+//       throw Exception(
+//           error.response?.data['error']['message'] ?? 'Unknown error');
+//     }
+//   }
+// }
+
+class BooksRemoteDataSource {
+  Future<Map<String, dynamic>> getBooksData({
+    required String bookName,
+  }) async {
+    final response = await Dio()
+        .get<Map<String, dynamic>>('http://openlibrary.org/search.json?q=lord');
+    print(response.data);
+    return response.data!;
+  }
+}
